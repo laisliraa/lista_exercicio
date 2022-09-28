007a4ee2beca0bcc84560b5f99b18ecc97ffa5d3
Lais Lira <laislirap04@hotmail.com>
2022-09-28 14:18:52 -0300

    Criação de programa de adição e multiplicação.

---
+++ ProgramaSoma.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ProgramaSoma {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        //variáveis utilizadas
+        int a;
+        int b;
+        int c;
+
+        //comandos de entrada para o usuário inserir informações
+        System.out.println("Digite um número:");
+        a = input.nextInt();
+
+        System.out.println("Digite um número novamente:");
+        b = input.nextInt();
+
+        //calculando as variáveis
+        if (a>b){
+            c = a + b;
+            System.out.println("A soma entre os números " + a + " e " + b + " é: " + c);
+        }else if (a<b) {
+            c = a * b;
+            System.out.println("A multiplicação entre os números " + a + " e " + b + " é: " + c);
+        }
+        
+        // se os números forem iguais : else System.out.println("Erro: Os nomes são iguais.");
+
+        input.close();
+    }
+}
