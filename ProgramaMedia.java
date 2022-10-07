e9af3b0adee9d680c1c21410ab3b261269aec739
Lais Lira <laislirap04@hotmail.com>
2022-10-07 10:42:51 -0300

    ajustes no if e else

--- ProgramaMedia.java
+++ ProgramaMedia.java
@@ -29,13 +29,13 @@ public class ProgramaMedia {
 
         //if e else 
          if (media >= 10){
-            System.out.println("O(a) aluno(a)" + nome_aluno +  "tem uma média A. Aprovado(a).");
+            System.out.println("O(a) aluno(a) " + nome_aluno +  "tem uma média A. Aprovado(a).");
          } else if (media >= 7 && media < 9){
-            System.out.println("O(a) aluno(a)" + nome_aluno + " tem uma média B. Aprovado(a).");
+            System.out.println("O(a) aluno(a) " + nome_aluno + " tem uma média B. Aprovado(a).");
          } else if (media >= 6 && media < 7){
-            System.out.println("O(a) aluno(a)" + nome_aluno + " tem uma média C. Aprovado(a).");
+            System.out.println("O(a) aluno(a) " + nome_aluno + " tem uma média C. Aprovado(a).");
          } else if (media >= 4 && media < 6){
-            System.out.println("O(a) aluno(a)" + nome_aluno + " tem uma média D. Reprovado(a).");
+            System.out.println("O(a) aluno(a) " + nome_aluno + " tem uma média D. Reprovado(a).");
          } else if (media < 4){
             System.out.println("O(a) aluno(a)" + nome_aluno + " tem uma média E. Reprovado(a).");
          }
