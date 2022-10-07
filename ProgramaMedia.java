410b670e33847d39e3cd66bf2ccfbdcdc43c1fdb
Lais Lira <laislirap04@hotmail.com>
2022-10-06 23:12:28 -0300

    adição da variável no if e else

--- ProgramaMedia.java
+++ ProgramaMedia.java
@@ -29,15 +29,15 @@ public class ProgramaMedia {
 
         //if e else 
          if (media >= 10){
-            System.out.println("O(a) aluno(a)" + "tem uma média A. Aprovado(a).");
+            System.out.println("O(a) aluno(a)" + nome_aluno +  "tem uma média A. Aprovado(a).");
          } else if (media >= 7 && media < 9){
-            System.out.println("O(a) aluno(a)" + "tem uma média B. Aprovado(a).");
+            System.out.println("O(a) aluno(a)" + nome_aluno + " tem uma média B. Aprovado(a).");
          } else if (media >= 6 && media < 7){
-            System.out.println("O(a) aluno(a)" + "tem uma média C. Aprovado(a).");
+            System.out.println("O(a) aluno(a)" + nome_aluno + " tem uma média C. Aprovado(a).");
          } else if (media >= 4 && media < 6){
-            System.out.println("O(a) aluno(a)" + "tem uma média D. Reprovado(a).");
+            System.out.println("O(a) aluno(a)" + nome_aluno + " tem uma média D. Reprovado(a).");
          } else if (media < 4){
-            System.out.println("O(a) aluno(a)" + "tem uma média E. Reprovado(a).");
+            System.out.println("O(a) aluno(a)" + nome_aluno + " tem uma média E. Reprovado(a).");
          }
 
         input.close();
